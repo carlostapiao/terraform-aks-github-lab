365be7bbbbacf7c1aba075782490a62a3cbb8d2b
Carlos Tapia <carlostapiat3csup32@gmail.com>
2026-03-16 22:52:32 +0000

    AKS terraform lab

---
+++ terraform/main.tf
@@ -0,0 +1,25 @@
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = var.aks_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "aks-lab"
+
+  default_node_pool {
+    name       = "nodepool1"
+    node_count = 1
+    vm_size    = "Standard_B2ps_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    environment = "lab"
+  }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "location" {
+  default = "centralus"
+}
+
+variable "resource_group_name" {
+  default = "rg-k8s-lab1"
+}
+
+variable "aks_name" {
+  default = "aks-lab"
+}
